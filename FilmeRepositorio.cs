aaa67ac40b13b3d197a033c323f7426524f32c7b
CUstela2 <vinicosta9015@gmail.com>
2025-08-25 14:36:27 -0300

    Update FilmeRepositorio.cs

--- FilmeRepositorio.cs
+++ FilmeRepositorio.cs
@@ -2,12 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace SQL_com_C_
 {
-    using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+
 
 public class FilmeRepositorio
 {
